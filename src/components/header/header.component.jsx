07bb843dd84ca0fb085566b5fdb9f15f37926552
Pradeep-M-S <pradeepms200076@gmail.com>
2020-07-22 21:04:14 +0530

    added toggle for cartIcon and implemented cartReducer in redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,14 +6,13 @@ import { auth } from "../../firebase/firebase.utils";
 
 import "./header.styles.scss";
 import { ReactComponent as Logo } from "../../assets/icon2.svg";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo />
-    </Link>
-    <Link className="brandName-container" to="/">
-      Claw Clothing
     </Link>
     <div className="options-container">
       <Link className="option" to="/shop">
@@ -31,12 +30,15 @@ const Header = ({ currentUser }) => (
           Sign In
         </Link>
       )}
+      <CartIcon />
     </div>
+    {hidden ? null : <CartDropdown />}
   </div>
 );
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 });
 
 export default connect(mapStateToProps)(Header);
